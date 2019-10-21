4c9cf348773e055ad43d4aad9ec2a09b7f098297
priya mirchandani <priyamirchandani1@gmail.com>
2019-10-21 12:23:11 -0700

    modified hello components for testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { Hello1Component } from './hello1/hello1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    Hello1Component
   ],
   imports: [
     BrowserModule,
